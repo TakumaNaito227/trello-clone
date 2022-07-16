26debafd9c73df5f4c376e11dc4ca8725459036a
TakumaNaito227 <63332756+TakumaNaito227@users.noreply.github.com>
2022-07-16 17:40:51 +0900

    add TaskCardTitle

--- src/components/task/TaskCardTitle.jsx
+++ src/components/task/TaskCardTitle.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 const TaskCardTitle = () => {
-  return (
-    <div>
-    </div>
-  )
-}
+  const [isClick, setIsClick] = useState(false);
+  const [inputCardTitle, setinputCardTitle] = useState("today");
 
-export default TaskCardTitle
+  const handleClick = () => {
+    setIsClick(true);
+  };
+
+  const handleChange = (e) => {
+    console.log(inputCardTitle);
+    setinputCardTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    // enterを押してもページが更新されない様に制御
+    e.preventDefault();
+    setIsClick(false);
+  };
+
+  return (
+    <div onClick={handleClick}>
+      {isClick ? (
+        // onSubmit -> enterを押したら起こるイベント処理
+        <form onSubmit={handleSubmit}>
+          {/* // onChange ->  フォーム内のエレメント（要素）の内容が変更された時に起こるイベント処理*/}
+          <input type="text" onChange={handleChange} />
+        </form>
+      ) : (
+        <h3>{inputCardTitle}</h3>
+      )}
+    </div>
+  );
+};
+
+export default TaskCardTitle;
