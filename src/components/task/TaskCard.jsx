f6ba306636d9c8e1cb15a61ca91e8ec14a0936fd
TakumaNaito227 <63332756+TakumaNaito227@users.noreply.github.com>
2022-07-19 16:59:43 +0900

    taskAddInput(WIP)

--- src/components/task/TaskCard.jsx
+++ src/components/task/TaskCard.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import TaskAddInput from './input/TaskAddInput'
-import TaskCardDeleteButton from './TaskCardDeleteButton'
-import TaskCardTitle from './TaskCardTitle'
-import Tasks from './Tasks'
+import React, { useState } from "react";
+import { useCallback } from "react";
+import TaskAddInput from "./input/TaskAddInput";
+import TaskCardDeleteButton from "./TaskCardDeleteButton";
+import TaskCardTitle from "./TaskCardTitle";
+import Tasks from "./Tasks";
 
 export default function TaskCard() {
+  conat[(inputText, setInputText)] = useState("");
+  conat[(taskList, setTaskList)] = useState([]);
   return (
-    <div className='taskCard'>
+    <div className="taskCard">
       <TaskCardTitle />
       <TaskCardDeleteButton />
-      <TaskAddInput />
+      <TaskAddInput
+        inputText={inputText}
+        setInputText={setInputText}
+        setTextList={setTaskList}
+        taskList={taskList}
+      />
       <Tasks />
     </div>
-  )
+  );
 }
--- src/components/task/input/TaskAddInput.jsx
+++ src/components/task/input/TaskAddInput.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 
-const TaskAddInput = () => {
+const TaskAddInput = ((inputText, setInputText, setTaskList, taskList)) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(e);
   };
 
+  const handleChange = (e) => {
+    setInputText(e.target.value)
+    console.log(inputText)
+  }
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="add a task" className="taskAddInput"/>
+        <input type="text" placeholder="add a task" className="taskAddInput" onChange={handleChange}/>
       </form>
     </div>
   );
